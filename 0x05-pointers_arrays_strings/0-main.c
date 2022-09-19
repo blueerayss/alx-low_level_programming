8d25f3001bb737c9f7e68d554b1a69cd033be9f2
blueerayss <hawwaalameen@gmail.com>
2022-09-19 15:00:21 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - check the code 
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = 402;
+	_putchar(n);
+	reset_to_98(&n);
+	_putchar(n);
+	return (0);
+}
